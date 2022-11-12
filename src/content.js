751343e9a20354da1566c2097f565d995f490dc8
aryan02420 <aryan02420@gmail.com>
2022-11-12 09:28:35 +0000

    fix: load info on problem bank button

--- src/content.js
+++ src/content.js
@@ -35,7 +35,6 @@ if (checks()) {
 		students: '<svg aria-hidden="true" focusable="false" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512"><path fill="currentColor" d="M144 160c-44.2 0-80-35.8-80-80S99.8 0 144 0s80 35.8 80 80s-35.8 80-80 80zm368 0c-44.2 0-80-35.8-80-80s35.8-80 80-80s80 35.8 80 80s-35.8 80-80 80zM0 298.7C0 239.8 47.8 192 106.7 192h42.7c15.9 0 31 3.5 44.6 9.7c-1.3 7.2-1.9 14.7-1.9 22.3c0 38.2 16.8 72.5 43.3 96c-.2 0-.4 0-.7 0H21.3C9.6 320 0 310.4 0 298.7zM405.3 320c-.2 0-.4 0-.7 0c26.6-23.5 43.3-57.8 43.3-96c0-7.6-.7-15-1.9-22.3c13.6-6.3 28.7-9.7 44.6-9.7h42.7C592.2 192 640 239.8 640 298.7c0 11.8-9.6 21.3-21.3 21.3H405.3zM416 224c0 53-43 96-96 96s-96-43-96-96s43-96 96-96s96 43 96 96zM128 485.3C128 411.7 187.7 352 261.3 352H378.7C452.3 352 512 411.7 512 485.3c0 14.7-11.9 26.7-26.7 26.7H154.7c-14.7 0-26.7-11.9-26.7-26.7z"/></svg>',
 		projects: '<svg aria-hidden="true" focusable="false" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M184 48H328c4.4 0 8 3.6 8 8V96H176V56c0-4.4 3.6-8 8-8zm-56 8V96H64C28.7 96 0 124.7 0 160v96H192 320 512V160c0-35.3-28.7-64-64-64H384V56c0-30.9-25.1-56-56-56H184c-30.9 0-56 25.1-56 56zM512 288H320v32c0 17.7-14.3 32-32 32H224c-17.7 0-32-14.3-32-32V288H0V416c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V288z"/></svg>',
 		discipline: '<svg aria-hidden="true" focusable="false" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512"><path fill="currentColor" d="M175 389.4c-9.8 16-15 34.3-15 53.1c-10 3.5-20.8 5.5-32 5.5c-53 0-96-43-96-96V64C14.3 64 0 49.7 0 32S14.3 0 32 0H96h64 64c17.7 0 32 14.3 32 32s-14.3 32-32 32V309.9l-49 79.6zM96 64v96h64V64H96zM352 0H480h32c17.7 0 32 14.3 32 32s-14.3 32-32 32V214.9L629.7 406.2c6.7 10.9 10.3 23.5 10.3 36.4c0 38.3-31.1 69.4-69.4 69.4H261.4c-38.3 0-69.4-31.1-69.4-69.4c0-12.8 3.6-25.4 10.3-36.4L320 214.9V64c-17.7 0-32-14.3-32-32s14.3-32 32-32h32zm32 64V224c0 5.9-1.6 11.7-4.7 16.8L330.5 320h171l-48.8-79.2c-3.1-5-4.7-10.8-4.7-16.8V64H384z"/></svg>',
-		grade: '<svg aria-hidden="true" focusable="false" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512"><path fill="currentColor" d="M353.8 54.1L330.2 6.3c-3.9-8.3-16.1-8.6-20.4 0L286.2 54.1l-52.3 7.5c-9.3 1.4-13.3 12.9-6.4 19.8l38 37-9 52.1c-1.4 9.3 8.2 16.5 16.8 12.2l46.9-24.8 46.6 24.4c8.6 4.3 18.3-2.9 16.8-12.2l-9-52.1 38-36.6c6.8-6.8 2.9-18.3-6.4-19.8l-52.3-7.5zM256 256c-17.7 0-32 14.3-32 32V480c0 17.7 14.3 32 32 32H384c17.7 0 32-14.3 32-32V288c0-17.7-14.3-32-32-32H256zM32 320c-17.7 0-32 14.3-32 32V480c0 17.7 14.3 32 32 32H160c17.7 0 32-14.3 32-32V352c0-17.7-14.3-32-32-32H32zm416 96v64c0 17.7 14.3 32 32 32H608c17.7 0 32-14.3 32-32V416c0-17.7-14.3-32-32-32H480c-17.7 0-32 14.3-32 32z"/></svg>'
 	}
 
 	const styles = `
@@ -220,7 +219,6 @@ if (checks()) {
 		<div id="__PSZY_STUDENTS__" title="#students" class="btn btn-white btn-toolbar btn-xs">${PSZYIcons.students} <span>-</span></div>
 		<div id="__PSZY_PROJECTS__" title="#projects" class="btn btn-white btn-toolbar btn-xs">${PSZYIcons.projects} <span>-</span></div>
 		<div id="__PSZY_DISCIPLINE__" title="disciplines" class="btn btn-white btn-toolbar btn-xs">${PSZYIcons.discipline} <span>-</span></div>
-		<div id="__PSZY_GRADE__" title="min cgpa" class="btn btn-white btn-toolbar btn-xs">${PSZYIcons.grade} <span>-</span></div>
 	</div>
 	<div class="spacer">&nbsp;</div>
 	<div id="__PSZY_CONTROLS__">
@@ -258,9 +256,9 @@ if (checks()) {
 			case '__PSZY_MOVETO__':
 				moveto(e.target.parentNode.parentNode)
 				break
-			case '__PSZY_PBANK__':{ 
+			case '__PSZY_PBANK__': {
 				let stid = e.target.parentNode.parentNode.querySelector('.spanclass.uiicon').attributes.spn.value
-				let fetchBody = {StationId: stid}
+				let fetchBody = { StationId: stid }
 				fetch("http://psd.bits-pilani.ac.in/Student/ViewActiveStationProblemBankData.aspx/getPBPOPUP", {
 					headers: {
 						"content-type": "application/json; charset=UTF-8",
@@ -276,20 +274,20 @@ if (checks()) {
 				.then(data => {
 					const parsed = JSON.parse(data.d)[0]
 					window.open(`StationproblemBankDetails.aspx?CompanyId=${parsed.CompanyId}&StationId=${parsed.StationId}&BatchIdFor=${parsed.BatchIdFor}&PSTypeFor=${parsed.PSTypeFor}`, "_blank")
+				}).then(() => {
+					updateStationInfo(e.target.parentNode.parentNode)
 				})
-				updateStationInfo(e.target.parentNode.parentNode).catch(e => console.error(JSON.stringify(e)))
 				break
 			}
 			case '__PSZY_STIPEND__':
 			case '__PSZY_STUDENTS__':
 			case '__PSZY_PROJECTS__':
 			case '__PSZY_DISCIPLINE__':
-			case '__PSZY_GRADE__':
-				updateStationInfo(e.target.parentNode.parentNode).catch(e => console.error(JSON.stringify(e)))
+				// updateStationInfo(e.target.parentNode.parentNode)
 				break
-			case '__PSZY_FETCHINFO__':{ 
+			case '__PSZY_FETCHINFO__': {
 				const allNodes = getAllItems()
-				allNodes.forEach(n => updateStationInfo(n).catch(e => console.error(JSON.stringify(e))))
+				allNodes.forEach(n => updateStationInfo(n))
 				break
 			}
 			case '__PSZY_EXPORT__':
@@ -582,12 +580,18 @@ if (checks()) {
 		})
 	}
 
-	async function updateStationInfo(node) {
+	function updateStationInfo(node) {
 		const stid = node.querySelector('.spanclass.uiicon').attributes.spn.value
-		const fetchBody = {StationId: stid}
-		const response = await fetch("http://psd.bits-pilani.ac.in/Student/ViewActiveStationProblemBankData.aspx/getPBPOPUP", {
+		const fetchBody = { StationId: stid }
+		fetch("http://psd.bits-pilani.ac.in/Student/ViewActiveStationProblemBankData.aspx/getPBPOPUP", {
 			headers: {
-				"content-type": "application/json; charset=UTF-8"
+				"accept": "application/json, text/javascript, */*; q=0.01",
+				"accept-language": "en-US,en;q=0.9",
+				"content-type": "application/json; charset=UTF-8",
+				"sec-gpc": "1",
+				"x-requested-with": "XMLHttpRequest",
+				"cache-control": "no-cache",
+				"pragma": "no-cache",
 			},
 			referrer: "http://psd.bits-pilani.ac.in/Student/ViewActiveStationProblemBankData.aspx",
 			referrerPolicy: "strict-origin-when-cross-origin",
@@ -596,41 +600,56 @@ if (checks()) {
 			mode: "cors",
 			credentials: "include"
 		})
-		if (!response.ok) throw new Error(await response.json())
-		const data = await response.json()
+		.then(response => response.json())
+		.then(data => {
+			console.log()
 		const parsed = JSON.parse(data.d)[0]
-		const response1 = await fetch("http://psd.bits-pilani.ac.in/Student/StationproblemBankDetails.aspx/ViewPB", {
+			const response1 = fetch("http://psd.bits-pilani.ac.in/Student/StationproblemBankDetails.aspx/ViewPB", {
 			headers: {
-				"content-type": "application/json; charset=UTF-8"
+					"accept": "application/json, text/javascript, */*; q=0.01",
+					"accept-language": "en-US,en;q=0.9",
+					"content-type": "application/json; charset=UTF-8",
+					"sec-gpc": "1",
+					"x-requested-with": "XMLHttpRequest",
+					"cache-control": "no-cache",
+					"pragma": "no-cache",
 			},
 			referrer: `http://psd.bits-pilani.ac.in/Student/StationproblemBankDetails.aspx?CompanyId=${parsed.CompanyId}&StationId=${parsed.StationId}&BatchIdFor=${parsed.BatchIdFor}&PSTypeFor=${parsed.PSTypeFor}`,
 			referrerPolicy: "strict-origin-when-cross-origin",
-			body: "{batchid: \"undefined\" }",
+				body: "{\"batchid\": \"undefined\" }",
 			method: "POST",
 			mode: "cors",
 			credentials: "include"
 		})
-		if (!response1.ok) throw new Error(await response1.json())
-		const data1 = await response1.json()
-		const parsed1 = JSON.parse(data1.d)[0]
-		const response2 = await fetch("http://psd.bits-pilani.ac.in/Student/StationproblemBankDetails.aspx/StationFacilitiesInfo", {
+			const response2 = fetch("http://psd.bits-pilani.ac.in/Student/StationproblemBankDetails.aspx/StationFacilitiesInfo", {
 			headers: {
-				"content-type": "application/json; charset=UTF-8"
+					"accept": "application/json, text/javascript, */*; q=0.01",
+					"accept-language": "en-US,en;q=0.9",
+					"content-type": "application/json; charset=UTF-8",
+					"sec-gpc": "1",
+					"x-requested-with": "XMLHttpRequest",
+					"cache-control": "no-cache",
+					"pragma": "no-cache",
 			},
 			referrer: `http://psd.bits-pilani.ac.in/Student/StationproblemBankDetails.aspx?CompanyId=${parsed.CompanyId}&StationId=${parsed.StationId}&BatchIdFor=${parsed.BatchIdFor}&PSTypeFor=${parsed.PSTypeFor}`,
 			referrerPolicy: "strict-origin-when-cross-origin",
-			body: "{StationId: \"0\"}",
+				body: "{\"StationId\": \"0\"}",
 			method: "POST",
 			mode: "cors",
 			credentials: "include"
 		})
-		if (!response2.ok) throw new Error(await response2.json())
-		const data2 = await response2.json()
+			return Promise.all([response1, response2])
+		})
+		.then(([response1, response2]) => Promise.all([response1.json(), response2.json()]))
+		.then(([data1, data2]) => {
+			const parsed1 = JSON.parse(data1.d)
 		const parsed2 = JSON.parse(data2.d)[0]
+			totStudents = parsed1?.map(p => p.TotalReqdStudents).reduce((acc, val) => acc + val) ?? '-'
+			tags = parsed1?.map(p => p.Tags).join(',').replaceAll(' ', '').replaceAll('Any', '') || 'Any'
 		node.querySelector('#__PSZY_STIPEND__ span').innerText = parsed2?.Stipend ?? '-'
-		node.querySelector('#__PSZY_STUDENTS__ span').innerText = parsed1?.TotalReqdStudents ?? '-'
-		node.querySelector('#__PSZY_PROJECTS__ span').innerText = parsed1?.TotalProject ?? '-'
-		node.querySelector('#__PSZY_DISCIPLINE__ span').innerText = Array.from(new Set((parsed1?.Tags.replaceAll(' ', '').replaceAll('Any', '') || 'Any').split(','))).join(',') 
-		node.querySelector('#__PSZY_GRADE__ span').innerText = parsed1?.GeneralMinCGPA ?? '-'
+			node.querySelector('#__PSZY_STUDENTS__ span').innerText = totStudents
+			node.querySelector('#__PSZY_PROJECTS__ span').innerText = parsed1?.[0].TotalProject ?? '-'
+			node.querySelector('#__PSZY_DISCIPLINE__ span').innerText = Array.from(new Set(tags.split(','))).join(',')
+		})
 	}
 }
