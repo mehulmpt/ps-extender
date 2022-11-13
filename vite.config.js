96aafad60474378593bd504e3241b1aafcd1f563
aryan02420 <aryan02420@gmail.com>
2022-11-13 08:32:02 +0000

    feat: add source mapping

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   build: {
+    sourcemap: 'inline',
     lib: {
       entry: resolve(__dirname, 'src/content.ts'),
       fileName: 'content',
